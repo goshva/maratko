82224f4f27db4094ff61c776fbdfe5e88479f76e
goshva <sha.egor@gmail.com>
2023-08-11 14:24:47 +0300

    swich bot to group

--- js/main.js
+++ js/main.js
@@ -181,12 +181,12 @@
 
 function feedback(action, status) {
   //
-  let token = "5430048154:AAEFptLp8IdbKirOYJzzM3ekyTd2ibVLMNc";
+  let token = "5430048154:AAEFptLp8IdbKirOYJzzM3ekyTd2ibVLMNc";/* :TODO NOTSECURITY REWERITE */
   //use this for testing
-  let chat_id = "190404167";
+ //let chat_id = "190404167";
   //let chat_id = "1329475336";//Aram ID
   //use this for production
-  //var chat_id = "-915348868";
+  let chat_id = "-915348868";
   let user_phone = prompt('Введите телефон для контакта с вами');
   var msg = `${action} от ${user_phone}`;// from ${getCookie("@")}`;
   var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${msg}&parse_mode=html`;
